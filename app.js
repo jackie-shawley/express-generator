6776383803ee3801e2a603a25d1e806262d9faf6
jackie-shawley <jackiekopco@aol.com>
2021-09-07 13:17:23 -0400

    Cookies

--- app.js
+++ app.js
@@ -32,28 +32,38 @@ app.set('view engine', 'jade');
 app.use(logger('dev')); //created by Express Generator
 app.use(express.json()); //created by Express Generator
 app.use(express.urlencoded({ extended: false })); //created by Express Generator
-app.use(cookieParser()); //created by Express Generator
+app.use(cookieParser('12345-67890-09876-54321')); //created by Express Generator
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
+  if(!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
 
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //Buffer is a global class belonging to Node, and .from is a static method of it. .split and .toString belong to vanilla JavaScript
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
-    return next(); //authorized
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //Buffer is a global class belonging to Node, and .from is a static method of it. .split and .toString belong to vanilla JavaScript
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', {signed: true})
+      return next(); //authorized
+    } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if (req.signedCookies.user === 'admin') {
+      return next();
+    } else {
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 

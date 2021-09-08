7e3c606df705db02c6aeb9be1a531f31cfdf4a1a
jackie-shawley <jackiekopco@aol.com>
2021-09-08 18:11:05 -0400

    Passport JWT

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var createError = require('http-errors'); //created by Express Generator
 var express = require('express'); //created by Express Generator
 var path = require('path'); //created by Express Generator
-var cookieParser = require('cookie-parser'); //created by Express Generator
 var logger = require('morgan'); //created by Express Generator
-const session = require('express-session');
-const FileStore = require('session-file-store')(session); //when you have two sets of paramaters like this, the initial function returns a function, then it is immediately called in the second parameter list. we must do this to use the FileStore in our app
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index'); //created by Express Generator
 var usersRouter = require('./routes/users'); //created by Express Generator
@@ -17,7 +14,7 @@ const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
 
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -38,37 +35,14 @@ app.set('view engine', 'jade');
 app.use(logger('dev')); //created by Express Generator
 app.use(express.json()); //created by Express Generator
 app.use(express.urlencoded({ extended: false })); //created by Express Generator
-//app.use(cookieParser('12345-67890-09876-54321')); //created by Express Generator; the key passed in can be anything you want it to be
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter); 
 
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-      const err = new Error('You are not authenticated!');                    
-      err.status = 401;
-      return next(err);
-  } else {
-      return next();
-  }
-}
-
-app.use(auth);
-
 app.use(express.static(path.join(__dirname, 'public'))); //created by Express Generator
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
